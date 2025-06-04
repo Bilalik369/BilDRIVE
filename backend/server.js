992248ac1f12eea1f9e2271c2543d06c41c4cb63
Bilalik369 <bilal11iken@gmail.com>
2025-06-04 15:04:51 +0100

    ajout de la fonction de connexion à la base de données MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
-import express from "express"
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js"; 
 
-
-dotenv.config()
+dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-app.use(express.json())
-const PORT = process.env.PORT || 5000
-app.listen(PORT , ()=>{
-console.log(`Server running on port ${PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(` Serveur lancé sur le port ${PORT}`);
+});
