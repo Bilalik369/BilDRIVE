5aa752877dca29a4a94b2c6f539b144a87f679ef
Bilalik369 <bilal11iken@gmail.com>
2025-08-03 16:58:39 +0100

    ajout du thunk loginUser pour gérer la connexion avec asyncThunk

--- frontend/src/redux/slices/authSlice.js
+++ frontend/src/redux/slices/authSlice.js
@@ -1,11 +1,13 @@
-import {createSlice , createAsyncThunk} from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { authApi } from "../api/authApi"
 
 
-export const  loginUser = createAsyncThunk("auth/login" , async({email , password} ,{rejectWithValue} )=>{
-    try {
-        const response =  await
-    } catch (error) {
-        
-    }
-
-}) 
+export const loginUser = createAsyncThunk("auth/login", async ({ email, password }, { rejectWithValue }) => {
+  try {
+    const response = await authApi.login(email, password)
+    localStorage.setItem("token", response.data.token)
+    return response.data
+  } catch (error) {
+    return rejectWithValue(error.response?.data?.message || "Login failed")
+  }
+})
