3a5f6cb5a3e12a7e6c25f57eeb956eb295f0cb96
Bilalik369 <bilal11iken@gmail.com>
2025-08-07 15:03:36 +0100

    ajoute la page de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { store } from "./redux/store";
 import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login"
+
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/auth/register" element={<Register />} />
+          <Route path="/auth/login" element={<Login/>} />
+
           
           <Route path="*" element={<div>Home or 404</div>} />
         </Routes>
