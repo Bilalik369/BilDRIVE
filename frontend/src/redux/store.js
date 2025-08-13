86413f62fb84c0f32438728022db4734a1aba044
Bilalik369 <bilal11iken@gmail.com>
2025-08-13 14:43:48 +0100

    ajout du slice Redux pour la gestion des courses passager

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
+import rideSlice from "./slices/rideSlice"
 import authReducer from "./slices/authSlice"
+import driverSlice from "./slices/driverSlice"
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    ride: rideSlice,
+    driver: driverSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
